fcc34dc7b7e62f047e2bfa308eb22bd5fe9068bb
joelpdurham <joeldur@gmail.com>
2019-12-06 14:08:13 -0800

    wrote a test for first route

---
+++ __tests__/app.test.js
@@ -0,0 +1,13 @@
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('app routes', () => {
+    it('has a home route that asks what up', () => {
+        return request(app)
+            .get('/')
+            .then(res => {
+                expect(res.body).toEqual({ text: 'what up' });
+            });
+    });
+});
+
