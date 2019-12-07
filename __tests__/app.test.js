bfd9420301aced58eba1e856cbe1eb6bc16f2704
joelpdurham <joeldur@gmail.com>
2019-12-07 13:38:08 -0800

    wrote test for update book

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,15 +1,31 @@
 const request = require('supertest');
 const { app } = require('../lib/app');
+const mongoose = require('mongoose');
 require('../lib/utils/connect')();
 const Book = require('../lib/models/Book');
 
 describe('app routes', () => {
     let book;
+    let updatedBook;
+
     beforeAll(async() => {
         book = await Book.create({
             title: 'Dune',
             author: 'Frank Herbert',
             pages: 500
+            
+        });
+
+        updatedBook = await Book.findByIdAndUpdate(book._id, {
+            title: 'Dune',
+            author: 'Frank Herbert',
+            pages: 600
+        });
+    });
+
+    afterAll(async() => {
+        mongoose.connection.collections['books'].drop(function(err) {
+            console.log('collection dropped');
         });
     });
 
@@ -45,4 +61,18 @@ describe('app routes', () => {
                 });
             });
     });
+
+    it('has a route that gets book by id and updates', () => {
+        return request(app)
+            .get(`/book/${updatedBook._id}`)
+            .then(res => {
+                expect(res.body).toEqual({
+                    title: 'Dune',
+                    author: 'Frank Herbert',
+                    pages: 600
+                });
+            });
+    });
+
+    //it has a route that finds and delets.
 });
